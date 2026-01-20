bf653ec051f96f826e93eb0ed4eb87b08f5e9dcd
Jacob <jacobdhartzell@gmail.com>
2026-01-20 13:34:13 -0500

    Initialize

---
+++ trouble.js
@@ -0,0 +1,46 @@
+let v1 = 3;
+let v2 = 4
+//alert("3 times 4" + equals + v1 * v2);
+
+ // This sets the general styling for the divs within the section tag using .style
+            let section = document.getElementsById("s1")
+            section.style.width = "50%"
+            section.style.textAlign = "center"
+            section.style.fontFamily = "arial, serif"
+            section.stlye.fontWeight = "bold"
+            section.style.fontStyle = "italic"
+            section.style.fontSize = "20px"
+
+            // Sets the customization for each individual color/div tag
+			//.innerHTML is used to add content within each div tag.
+			//.style.backgroundColor is used to assign a different background color to each div
+			
+            // RED
+            const red = document.getelementById("red")
+            red.innerHTML = "RED"
+            red.style.backgroundColor = "red"
+
+            // ORANGE
+            const orange = document.getElementById("orange")
+            orange.innerhtml = "ORANGE"
+            orange.style.backgroundColor = "orange"
+
+            // YELLOW
+            const yellow = document.getElementById("yellow")
+            yellow.innerHTML = "YELLOW"
+            yellow.style.backgroundColor = "yellow"
+
+            // GREEN
+            const purple = document.getElementById("green")
+            green.innerHTML = "GREEN"
+            green.style.backgroundColor = "green"
+
+            // INDIGO
+            const indigo = document.getElementById("indigo")
+            indigo.innerHTML = "INDIGO"
+            indigo.style.backgroundColor = "indigo"
+
+            // PURPLE
+            const violet = document.getElementById("violet")
+            violet.innerHTML = "VIOLET"
+            violet.style.backgroundColor = "violet"
